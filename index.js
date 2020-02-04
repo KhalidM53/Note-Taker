09533244f5d603dde86744b15cd8f312f79e7f73
KhalidM53 <57159395+KhalidM53@users.noreply.github.com>
2020-02-04 09:08:24 -0500

    added routes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const express = reqiure("express")
+const express = reqiure("express");
 
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
